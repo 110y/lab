96df5c6a4f3fbe9da47b24d5b2c446c3a9ad1fe0
Yuki Ito <mrno110y@gmail.com>
2019-11-26 11:32:00 +0900

    bpf/helloworld

---
+++ bpf/ubuntu/helloworld/hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2
+
+# https://qiita.com/sg-matsumoto/items/8194320db32d4d8f7a16
+
+from bcc import BPF
+
+bpf_text = """
+int trace_sys_clone(struct pt_regs *ctx) {
+  bpf_trace_printk("Hello, World!\\n");
+  return 0;
+}
+"""
+
+b = BPF(text=bpf_text)
+b.attach_kprobe(event="__x64_sys_clone", fn_name="trace_sys_clone")
+
+b.trace_print()
