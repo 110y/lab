720cba1c290e2d433cb55ad8a43576a65b171a3c
Yuki Ito <mrno110y@gmail.com>
2020-01-31 14:30:36 +0900

    authority: iss

--- envoy/authority/handler/jwt.go
+++ envoy/authority/handler/jwt.go
@@ -10,12 +10,20 @@ import (
 
 var _ http.Handler = &JWT{}
 
+const issuer = "authority.authority.svc.cluster.local"
+
 type JWT struct {
 	PrivateKey *rsa.PrivateKey
 }
 
 func (j *JWT) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	token := jwt.New()
+
+	if err := token.Set(jwt.IssuerKey, issuer); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	signedToken, err := token.Sign(jwa.RS256, j.PrivateKey)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
